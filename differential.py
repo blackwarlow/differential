5cf8e4beb1285aaa396520fd534380bea5c0f47b
Nikita Belomestnykh <blackwarlow+github@gmail.com>
2023-07-14 12:13:08 +0300

    Update differential.py
    Add __future__.annotations support for python < 3.8

--- differential.py
+++ differential.py
@@ -22,6 +22,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+from __future__ import annotations
+
 import re
 from parser import *
 from typing import Any
